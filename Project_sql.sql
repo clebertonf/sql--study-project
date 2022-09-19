40a3fb32fca20438879f76f1e0cd91b41b849fb8
Cleberton Carvalho <clebertonfgc@gmail.com>
2022-09-19 10:09:43 -0300

    Add table Course

--- Project_sql.sql
+++ Project_sql.sql
@@ -35,9 +35,47 @@ CREATE TABLE [Career]
     [Title] NVARCHAR(160) NOT NULL,
     [Summary] NVARCHAR(2000) NOT NULL,
     [Url] NVARCHAR(1024) NOT NULL,
-    [DurationInMinutes] INT NOT NULL,
+    
     [Active] BIT NOT NULL,
     [Featured] BIT NOT NULL,
     [Tags] NVARCHAR(160) NOT NULL,
     CONSTRAINT [PK_Career] PRIMARY KEY ([Id])
 );
+GO
+
+CREATE TABLE [Category]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Order] INT NOT NULL,
+    [Description] TEXT NOT NULL,
+    [Featured] BIT NOT NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Course]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Tag] NVARCHAR(20) NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [Level] TINYINT NOT NULL,
+    [DurationInMinutes] INT NOT NULL,
+    [CreateDate] DATETIME NOT NULL,
+    [LastUpdate] DATETIME NOT NULL,
+    [Active] BIT NOT NULL,
+    [Free] INT NOT NULL,
+    [Featured] BIT NOT NULL,
+    [AuthorId] UNIQUEIDENTIFIER NULL,
+    [CategoryId] UNIQUEIDENTIFIER NULL,
+    [Tags] NVARCHAR(160) NOT NULL,
+    
+    CONSTRAINT [PK_Course] PRIMARY KEY([Id]),
+    CONSTRAINT [FK_Course_Author_AuthorID] FOREIGN KEY ([AuthorId]) REFERENCES [Author] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_Course_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id]) ON DELETE NO ACTION,
+);
+GO
