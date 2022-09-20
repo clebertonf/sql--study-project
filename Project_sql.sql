3e116350b2f7416ecfb572e08a9047e65869b456
Cleberton Carvalho <clebertonfgc@gmail.com>
2022-09-20 00:01:34 -0300

    Tabelas parte 2

--- Project_sql.sql
+++ Project_sql.sql
@@ -35,7 +35,6 @@ CREATE TABLE [Career]
     [Title] NVARCHAR(160) NOT NULL,
     [Summary] NVARCHAR(2000) NOT NULL,
     [Url] NVARCHAR(1024) NOT NULL,
-    
     [Active] BIT NOT NULL,
     [Featured] BIT NOT NULL,
     [Tags] NVARCHAR(160) NOT NULL,
@@ -79,3 +78,34 @@ CREATE TABLE [Course]
     CONSTRAINT [FK_Course_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id]) ON DELETE NO ACTION,
 );
 GO
+
+CREATE TABLE [CareerItem]
+(
+    [CareerID]  UNIQUEIDENTIFIER NULL,
+    [CourseId] UNIQUEIDENTIFIER NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Description] TEXT NOT NULL,
+    [Order] TINYINT NOT NULL,
+
+    CONSTRAINT [PK_CareerItem] PRIMARY KEY([CourseId], [CareerId]),
+    CONSTRAINT [FK_CareerItem_Career_CareerId] FOREIGN KEY ([CareerId]) REFERENCES [Career] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_CareerItem_Course_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Course] ([Id]) ON DELETE NO ACTION,
+);
+GO
+
+CREATE TABLE [StudentCourse]
+(
+    
+    [CourserID]  UNIQUEIDENTIFIER NOT NULL,
+    [StudentId] UNIQUEIDENTIFIER NOT NULL,
+    [Progress] TINYINT NOT NULL,
+    [Favorite] BIT NOT NULL,
+    [StartDate] DATETIME NOT NULL,
+    [LastUpdateDate] DATETIME NOT NULL,
+
+    CONSTRAINT [PK_StudentCourse] PRIMARY KEY([CourseId], [StudentId]),
+    CONSTRAINT [FK_StudentCourse_Course_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Course] ([Id]) ON DELETE NO ACTION,
+    CONSTRAINT [FK_studentCourse_Student_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [Student] ([Id]) ON DELETE NO ACTION,
+);
+GO
+
